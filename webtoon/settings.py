078ec509da56f10835a79aaaa98a7ad00070d68f
dbfla8772 <dbfla5527@naver.com>
2021-08-18 22:06:54 +0900

    heroku

--- webtoon/settings.py
+++ webtoon/settings.py
@@ -151,7 +151,7 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'dbfla87723@gmail.com'
-EMAIL_HOST_PASSWORD = 'dbfla2132@'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
